ce25ca041743225e7a3ae0134e89efb8e6bc7ed6
davidpropfin <56353788+davidpropfin@users.noreply.github.com>
2019-11-02 04:11:51 +0700

    lên

---
+++ ThinkInJava/src/main/java/DavidLe/ThinkInJava/App.java
@@ -0,0 +1,13 @@
+package DavidLe.ThinkInJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
